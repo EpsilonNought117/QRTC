c8b1caae722a388efae69c7cfc5523e35735496e
Devansh Mehrotra <devanshmehrotra117@gmail.com>
2026-04-08 17:17:44 +0530

    re-writing entire project

---
+++ headers/vec3.hpp
@@ -0,0 +1,14 @@
+#ifndef QRTC_VEC3_HPP
+#define QRTC_VEC3_HPP
+
+template <typename T>
+struct vec3
+{
+    T x, y, z;
+
+    
+};
+
+
+
+#endif
---
+++ include/qrtc.hpp
@@ -0,0 +1,8 @@
+#ifndef QRTC_HPP
+#define QRTC_HPP
+
+#include <iostream>
+#include <cstdint>
+#include <cmath>
+
+#endif
