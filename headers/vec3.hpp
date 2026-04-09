63ae8c44e52bf53afc2e551c33eb9ce2d84d9fcb
Devansh Mehrotra <devanshmehrotra117@gmail.com>
2026-04-09 19:48:59 +0530

    adding SIMD support for x86-64

--- headers/vec3.hpp
+++ headers/vec3.hpp
@@ -1,13 +1,31 @@
 #ifndef QRTC_VEC3_HPP
 #define QRTC_VEC3_HPP
 
+#include "../include/qrtc.hpp"
+
 template <typename T>
 struct vec3
 {
     T x, y, z;
 
-    
+
 };
+
+#if defined(QRTC_FLOAT32)
+
+#if defined(QRTC_AMD64_SSE2) || defined(QRTC_AMD64_DYNAMIC_DISPATCH)
+
+
+
+#endif
+
+#elif defined(QRTC_FLOAT64)
+
+
+
+#else
+    #error "QRTC can only work with 64-bit or 32-bit floats!"
+#endif
 
 
 
--- include/qrtc.hpp
+++ include/qrtc.hpp
@@ -4,5 +4,82 @@
 #include <iostream>
 #include <cstdint>
 #include <cmath>
+#include <fstream>
+#include <thread>
+#include <atomic>
 
+/****************************************************************************************************/
+/******************   COMPILER SPECIFIC HEADERS AND DLL/STATIC IMPORT/EXPORTS    ********************/
+/****************************************************************************************************/
+
+#if defined(_WIN32)
+
+    #if defined(_MSC_VER)
+    
+        #if defined(_M_X64) || defined(_M_AMD64)
+        
+            #include <immintrin.h>
+            #include <intrin.h>
+        
+        #elif defined(_M_ARM64) || defined(_M_ARM64EC)
+        
+            // TODO
+        
+        #else
+            #error "Unsupported Architecture on Windows and MSVC!"
+        #endif
+    
+    #else
+        #error "Unknown Compiler on Windows!"
+    #endif
+
+    #if defined(QRTC_BUILD_SHARED_LIB)
+
+        #define QRTC_API __declspec(dllexport)
+    
+    #elif defined(QRTC_SHARED)
+    
+        #define QRTC_API __declspec(dllimport)
+
+    #else
+        #define QRTC_API
+    #endif
+
+#elif defined(__linux__) || defined(__linux)   || \
+      defined(__unix__)  || defined(__unix)    || \
+      defined(__APPLE__) || defined(__MACH__)
+
+    #if defined(__GNUC__) || defined(__clang__)
+        
+        #if defined(__x86_64__)  || defined(__amd64__)  || \
+            defined(__x86_64)    || defined(__amd64)
+        
+            #include <cpuid.h>
+            #include <immintrin.h>
+        
+        #elif defined(__aarch64__) || defined(__arm64__)
+        
+            #include <arm_acle.h>
+            #include <arm_neon.h>
+        
+        #else
+            #error "Unsupported Architecture on Linux/Unix/macOS and GCC/Clang!"
+        #endif
+
+    #else
+        #error "Unknown Compiler on Linux/Unix/macOS!"
+    #endif
+
+    #if defined(QRTC_BUILD_SHARED_LIB)
+    
+        #define QRTC_API __attribute__((visibility("default")))
+    
+    #else
+        #define QRTC_API
+    #endif
+
+#else
+    #error "Unknown Platform!"
 #endif
+
+#endif // QRTC_HPP
