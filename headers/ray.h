0d9d7d0f21187a568fd73bbfc81e85e8ae59969d
Devansh Mehrotra <devanshmehrotra117@gmail.com>
2026-04-22 18:55:41 +0530

    feat: added Ray type

--- headers/ray.h
+++ headers/ray.h
@@ -1,6 +1,22 @@
 #ifndef QRTC_RAY_H
 #define QRTC_RAY_H
 
+#include "../include/qrtc.h"
+#include "vec3.h"
 
+typedef struct Ray
+{
+    Point3 orig;
+    Vec3 dir;
+
+} Ray;
+
+
+static inline Point3 EvalRayAt(const Ray* r, float t /* parameter */)
+{
+    Point3 p = MulVec3Scalar(&(r->dir), t);
+    AddVec3Inplace(&p, &(r->orig));
+    return p;
+}
 
 #endif
